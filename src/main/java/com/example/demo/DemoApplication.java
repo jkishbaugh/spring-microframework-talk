e120026e62eabf4fba2daff1c62ee8b2908c3497
Alexey Nesterov <alexey.nesterov@live.com>
2020-05-10 16:33:25 +0100

    Functional bean definitions

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.support.GenericApplicationContext;
 
-@SpringBootApplication(proxyBeanMethods = false)
+@SpringBootConfiguration(proxyBeanMethods = false)
+@EnableAutoConfiguration
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static SpringApplication buildApp() {
+        var translationService = new TranslationService();
+
+        return new SpringApplicationBuilder(DemoApplication.class)
+                .initializers((GenericApplicationContext applicationContext) -> {
+                    applicationContext.registerBean(BookController.class,
+                            () -> new BookController(applicationContext.getBean(BookRepository.class), translationService));
+                    // or applicationContext.registerBean(BookController.class) to delegate it back to Spring
+
+                    applicationContext.registerBean(TranslationService.class,
+                            () -> translationService);
+                })
+                .build();
+    }
+
+    public static void main(String[] args) {
+        buildApp().run(args);
+    }
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,34 +2,41 @@ package com.example.demo;
 
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.AutoConfigureDataR2dbc;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static com.example.demo.DemoApplication.buildApp;
 import static org.hamcrest.Matchers.equalTo;
 
-@SpringBootTest
-@AutoConfigureWebTestClient
-@AutoConfigureDataR2dbc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DemoApplicationTests {
 
-    @Autowired
-    WebTestClient webTestClient;
-
-    @Autowired
-    DatabaseClient databaseClient;
+    private ConfigurableApplicationContext applicationContext;
+    private WebTestClient webTestClient;
 
     @BeforeAll
     void setUp() {
+        applicationContext = buildApp().run();
+        webTestClient = WebTestClient.bindToApplicationContext(applicationContext).build();
+
+        DatabaseClient databaseClient = applicationContext.getBean(DatabaseClient.class);
         databaseClient.execute("create table if not exists book (id int auto_increment primary key, title varchar );").then()
                 .then(databaseClient.execute("insert into book (title) values ('A year in Provence')").then())
                 .then()
                 .block();
+    }
+
+    @AfterAll
+    void tearDown() {
+        applicationContext.close();
     }
 
     @Test
